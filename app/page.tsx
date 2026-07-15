1ca2bfe39ab3adbaf7d05f2302bc403444f6e53f
fukashigiNo <zemvikak@gmail.com>
2026-07-15 19:40:05 +0500

    main part of app

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Hero } from "@/widgets/hero";
+import { Challanges } from "@/widgets/challenges";
+import {News} from "@/widgets/news"
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Challanges />
+      <News />
+    </>
+  )
+}
